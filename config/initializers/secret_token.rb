7d27584f7bddd0e2ec88f48ac5e871b7cdb73e3a
evx001 <eviexeris@yahoo.com>
2012-08-06 02:53:15 -0400

    1st commit no-bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamanthaCMS::Application.config.secret_token = 'f5ee729f04c27a0b094b7457c11b86d4a640292c25802aa92b8ed837a8e93059262d7ca4bae4d1ffa10720ad718493c8169321378db76cfd6d3ecff6461170cf'
